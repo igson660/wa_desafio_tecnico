dda0f53cdfc151bd6b6d178f5cbbcbb674bafbe8
igson <igson660@gmail.com>
2021-12-18 16:29:34 -0500

    add score in checkout page

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import path from '../Paths';
 import{ Container, Button } from '../styles/checkoutPage';
 
 
 function CheckoutPage() {
+  const restart = () => {
+    localStorage.removeItem('quantityQuestions');
+    localStorage.removeItem('answers');
+  }
+
+  const [note, setNote] = useState(0);
+
+  useEffect(() => {
+    const insertNote = async () => {
+      let note = 0;
+      const questions = JSON.parse(localStorage.getItem('answers'));
+
+      questions.forEach((question) => {
+        if(question.selectAnswer === question.correct_answer) {
+          if(question.difficulty === 'easy') {
+            note += 1;
+          } else if(question.difficulty === 'medium') {
+            note += 2;
+          } else {
+            note += 3;
+          }
+        }
+      });
+  
+      setNote(note)
+    }
+
+    insertNote();
+  }, [note]);
 
   return (
     <Container>
       <main>
-        <h1>Congratulations your grade is </h1>
+        <h1>{ `congratulations your score is: ${ note }` } </h1>
         <Link to={ path.homePage} >
-          <Button>Restart</Button>
+          <Button
+            onClick={ restart }
+          >Restart</Button>
         </Link>
       </main>
     </Container>
